ab3907f3636b59762d44172b60b65bd6857c07b4
temeboss <107496448+temeboss@users.noreply.github.com>
2022-07-08 16:43:09 +0300

    LAB

---
+++ labGUI 1/labGUI 1/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace labGUI_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_Number = new System.Windows.Forms.TextBox();
+            this.txt_Inventory = new System.Windows.Forms.TextBox();
+            this.txt_object = new System.Windows.Forms.TextBox();
+            this.txt_Count = new System.Windows.Forms.TextBox();
+            this.txt_date = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Price = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(463, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(610, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Inventory number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "object Number";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Count ";
+            // 
+            // txt_Number
+            // 
+            this.txt_Number.Location = new System.Drawing.Point(68, 86);
+            this.txt_Number.Name = "txt_Number";
+            this.txt_Number.Size = new System.Drawing.Size(176, 22);
+            this.txt_Number.TabIndex = 7;
+            this.txt_Number.TextChanged += new System.EventHandler(this.txt_Number_TextChanged);
+            // 
+            // txt_Inventory
+            // 
+            this.txt_Inventory.Location = new System.Drawing.Point(588, 86);
+            this.txt_Inventory.Name = "txt_Inventory";
+            this.txt_Inventory.Size = new System.Drawing.Size(176, 22);
+            this.txt_Inventory.TabIndex = 8;
+            this.txt_Inventory.TextChanged += new System.EventHandler(this.txt_Inventory_TextChanged);
+            // 
+            // txt_object
+            // 
+            this.txt_object.Location = new System.Drawing.Point(68, 175);
+            this.txt_object.Name = "txt_object";
+            this.txt_object.Size = new System.Drawing.Size(276, 22);
+            this.txt_object.TabIndex = 9;
+            this.txt_object.TextChanged += new System.EventHandler(this.txt_object_TextChanged);
+            // 
+            // txt_Count
+            // 
+            this.txt_Count.Location = new System.Drawing.Point(68, 254);
+            this.txt_Count.Name = "txt_Count";
+            this.txt_Count.Size = new System.Drawing.Size(194, 22);
+            this.txt_Count.TabIndex = 10;
+            this.txt_Count.TextChanged += new System.EventHandler(this.txt_Count_TextChanged);
+            // 
+            // txt_date
+            // 
+            this.txt_date.Location = new System.Drawing.Point(290, 86);
+            this.txt_date.Name = "txt_date";
+            this.txt_date.Size = new System.Drawing.Size(242, 22);
+            this.txt_date.TabIndex = 12;
+            this.txt_date.ValueChanged += new System.EventHandler(this.txt_date_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(495, 222);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Price";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // txt_Price
+            // 
+            this.txt_Price.Location = new System.Drawing.Point(498, 254);
+            this.txt_Price.Name = "txt_Price";
+            this.txt_Price.Size = new System.Drawing.Size(194, 22);
+            this.txt_Price.TabIndex = 14;
+            this.txt_Price.TextChanged += new System.EventHandler(this.txt_Price_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 421);
+            this.Controls.Add(this.txt_Price);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_date);
+            this.Controls.Add(this.txt_Count);
+            this.Controls.Add(this.txt_object);
+            this.Controls.Add(this.txt_Inventory);
+            this.Controls.Add(this.txt_Number);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Name = "Form1";
+            this.Text = "Inventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_Number;
+        private System.Windows.Forms.TextBox txt_Inventory;
+        private System.Windows.Forms.TextBox txt_object;
+        private System.Windows.Forms.TextBox txt_Count;
+        private System.Windows.Forms.DateTimePicker txt_date;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Price;
+    }
+}
+
---
+++ labGUI 1/labGUI 1/Properties/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labGUI_1.Properties
+{
+    internal class items 
+    {
+
+        public string number { get; set; }
+        public string inventory { get; set; }
+        public string objectname { get; set; }
+        public string count { get; set; }
+        public string price { get; set; }
+
+        public void save()
+        {
+            Console.WriteLine("saved");
+        }
+
+       
+
+    }
+}
