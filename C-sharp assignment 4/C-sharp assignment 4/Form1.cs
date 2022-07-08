08e229e35888b1f6a1db7382fb2f838c98a08397
temeboss <107496448+temeboss@users.noreply.github.com>
2022-07-08 16:35:48 +0300

    GUI

---
+++ C-sharp assignment 4/C-sharp assignment 4/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using labGUI_1.Properties;
+
+namespace labGUI_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            items c = new items
+            {
+                number = txt_Number.Text,
+                inventory = txt_Inventory.Text,
+                objectname = txt_object.Text,
+                count = txt_Count.Text,
+                price = txt_Price.Text,
+            };
+            MessageBox.Show("saved");
+
+        }
+
+        private void txt_Inventory_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txt_Number_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_date_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_object_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Count_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_Price_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
